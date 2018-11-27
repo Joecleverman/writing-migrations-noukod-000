a1378888d8e2df073efeb73f0128b0be0a12052a
Josue Abraham <josuea@noukod.com>
2018-11-27 17:05:16 +0000

    automatically backed up by learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,4 +1,4 @@
-class AddFavoriteFoodToArtists < ActiveRecord::Migration
+class CreateStudents < ActiveRecord::Migration[5.1]
   def change
     add_column :grade, :birthdate, :integer, :string
   end
